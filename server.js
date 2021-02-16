09495dbda4b2e03f67f52ac60714512d8465779d
Bethold <mtaycruz@gmail.com>
2021-02-15 21:27:29 -0500

    change port for heroku

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+
+const PORT = process.env.PORT || 3001;
 const express = require('express');
 const { animals } = require('./data/animals');
 
@@ -55,5 +57,5 @@ app.get('/api/animals', (req,res) => {
 });
 
 app.listen(3001, () => {
-    console.log(`API server now on port 3001`);
+    console.log(`API server now on port ${PORT}`);
 });
